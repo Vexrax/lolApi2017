bb30cfb34a6bbf3652862f798a3dbea839b9b319
Vexrax <joshuaf_111@live.com>
2017-12-28 22:19:46 -0500

    Bug fixes, alignment changes on summoner page

--- webjs/main.js
+++ webjs/main.js
@@ -1,9 +1,14 @@
 var socket = io();
 //console.log("data emitted");
 //socket.emit("getMatchHistory", "");
+var document = require('html-element').document;
 
 socket.on("matchHistory", function(summonerData, gamesList, currentRunesList, advisedRunesList) {
     console.log(summonerData);
     console.log(gamesList);
     console.log(currentRunesList);
+});
+
+socket.on("modifyHTML", function(data){
+    document.getElementById("nameplate").innerHTML = data;
 });
