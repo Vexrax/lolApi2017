13c18b4b186d92d59c2ff0ed2870f55e316f16f8
Vexrax <joshuaf_111@live.com>
2017-12-30 04:19:33 -0500

    Backend hooking into matchmaking done. Css adjustments.

--- server/mySQL.js
+++ server/mySQL.js
@@ -7,7 +7,7 @@ function SQL() {
         database: 'leaguedb'
     });
     this.connection.connect(function(err) {
-
+        if(err) throw err;
         console.log("Connected to Database");
     });
 }
